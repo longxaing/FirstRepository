ecf880d85e403e45d90196b99b5a04a3c1aced0f
sunhongen <522188762@qq.com>
2022-03-23 17:35:54 +0800

    master

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace first_test_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = true;
+            Type strType = str.GetType();
+            Console.WriteLine($"str type is {strType}");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
